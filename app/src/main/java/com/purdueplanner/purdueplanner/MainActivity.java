10e47c9be60b9e6279a3845bf358801c014f383b
Brandon Nice <niceb@purdue.edu>
2015-10-11 16:18:31 -0400

    Added a MapsActivity page

--- app/src/main/java/com/purdueplanner/purdueplanner/MainActivity.java
+++ app/src/main/java/com/purdueplanner/purdueplanner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.purdueplanner.purdueplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -43,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
