cab525070aca78aab3f77a3e081383a5e533b47d
Brandon Nice <niceb@purdue.edu>
2015-10-11 16:33:46 -0400

    Added a MapsActivity page

--- app/src/main/java/com/purdueplanner/purdueplanner/MainActivity.java
+++ app/src/main/java/com/purdueplanner/purdueplanner/MainActivity.java
@@ -1,6 +1,7 @@
 package com.purdueplanner.purdueplanner;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
        });*/
+
+        ImageButton mapButton = (ImageButton) findViewById(R.id.mapsButton);
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
     }
 
     @Override
@@ -49,4 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
