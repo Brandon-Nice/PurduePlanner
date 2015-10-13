edd561544e32f400a77d0ffd90d522150d673d12
menane <menane@purdue.edu>
2015-10-13 17:26:37 -0400

    Added two new classes: ScheduleActivity and FriendsActivity. Also added an xml file of the Schedule view

---
+++ app/src/main/java/com/purdueplanner/purdueplanner/ScheduleActivity.java
@@ -0,0 +1,9 @@
+package com.purdueplanner.purdueplanner;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by menane on 10/13/15.
+ */
+public class ScheduleActivity extends FragmentActivity{
+}
