7149fd5f280f703de2fac1a8e343ad8b0656543f
menane <menane@purdue.edu>
2015-10-13 17:39:18 -0400

    Implemented the onCreate method for ScheduleActivity

--- app/src/main/java/com/purdueplanner/purdueplanner/ScheduleActivity.java
+++ app/src/main/java/com/purdueplanner/purdueplanner/ScheduleActivity.java
@@ -1,9 +1,16 @@
 package com.purdueplanner.purdueplanner;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by menane on 10/13/15.
  */
 public class ScheduleActivity extends FragmentActivity{
+    @Override
+    //provide the onCreate method to apply the Schedule layout to the activity
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
 }
