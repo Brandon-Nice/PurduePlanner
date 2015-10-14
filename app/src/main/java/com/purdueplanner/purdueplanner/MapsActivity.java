26c47fad373cf9e3db958039d71aea8a28d8c7b1
Jake <jake.1995@sbcglobal.net>
2015-10-13 20:07:03 -0400

    test

--- app/src/main/java/com/purdueplanner/purdueplanner/MapsActivity.java
+++ app/src/main/java/com/purdueplanner/purdueplanner/MapsActivity.java
@@ -25,7 +25,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
+//test change
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
