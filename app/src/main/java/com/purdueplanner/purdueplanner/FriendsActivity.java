e1102ffa33aede51165e59067dbbc8a9aad63e65
Brandon Nice <niceb@purdue.edu>
2015-11-14 18:04:13 -0500

    Changed FriendsActivity to not crash and use a better method to get JSON object

--- app/src/main/java/com/purdueplanner/purdueplanner/FriendsActivity.java
+++ app/src/main/java/com/purdueplanner/purdueplanner/FriendsActivity.java
@@ -4,17 +4,25 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class FriendsActivity extends AppCompatActivity {
+    static LoginButton loginButton;
+    static CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,30 +32,42 @@ public class FriendsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        loginButton = LoginActivity.getLoginButton();
+        callbackManager = LoginActivity.getCallbackManager();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    object = response.getJSONObject();
+                                    Log.d("LoginActivity", response.toString());
+                                    String id = object.getString("id");
 
-        //Adds the Facebook friends
-        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new
-                GraphRequest.GraphJSONObjectCallback() {
-                @Override
-                public void onCompleted(JSONObject object, GraphResponse response) {
-                    //TODO: Have to decode the JSON object
-                    object = response.getJSONObject();
-                    try {
-                        if (object != null) {
-                            String user_id = object.getString("id");
-                            String user_name = object.getString("name");
-                            //TODO: Try to get friendlists
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
 
-                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "friendslists");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                e.printStackTrace();
+            }
         });
-        Bundle parameters = new Bundle();
-        parameters.putString("fields", "id,name,friendlists");
-        request.setParameters(parameters);
-        request.executeAsync();
 
 
     }
