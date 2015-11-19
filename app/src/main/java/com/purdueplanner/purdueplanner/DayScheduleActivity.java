dfea2bd7fe995da5f3a175ca9f43ba3503b42739
menane <menane@purdue.edu>
2015-11-18 23:57:34 -0500

    Finally when you click a date on the calendar, a new activity pops up displaying the classes you have for the day! (still has the debugging output on the Calendar view.. will take out later)

--- app/src/main/java/com/purdueplanner/purdueplanner/DayScheduleActivity.java
+++ app/src/main/java/com/purdueplanner/purdueplanner/DayScheduleActivity.java
@@ -1,7 +1,13 @@
 package com.purdueplanner.purdueplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -10,10 +16,38 @@ import android.os.Bundle;
 
 //What to do when a day is clicked on the calendar view
 public class DayScheduleActivity extends AppCompatActivity {
-
+    private ListView classesForDayListView;
+    private ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_schedule);
+
+        Intent myIntent = getIntent(); // gets the previously created intent
+//        String currentDate = myIntent.getStringExtra("currdate_key");
+        String dayLetter = myIntent.getStringExtra("dayletter_key"); // will return the value associated with this key, i.e. the day letter string
+
+//         Debugging shiz!
+//        TextView dateText = (TextView)findViewById(R.id.displayDate); //sets the date text to the one on our xml file
+//        dateText.setText(currentDate);
+//        TextView dayText = (TextView)findViewById(R.id.displayDay); //sets the day text to the one on our xml file
+//        dayText.setText(dayLetter);
+
+        //Get the actual classes for each day
+        ArrayList<Classes> classList = ((MyApplication) getApplication()).getStudent().getSchedule(); //total classes for the student
+        ArrayList<String> classPerDayList = new ArrayList<>(); //classes per each day for the student
+        int size = classList.size();
+        for (int i = 0; i < size; i++) { //iterates through the classes
+            if(classList.get(i).getDays().contains(dayLetter)){ //match by the day
+                classPerDayList.add(classList.get(i).getMajor() + " " + classList.get(i).getCourseNum() + " " + classList.get(i).getStartTime()
+                        + " - " + classList.get(i).getEndTime());
+            }
+        }
+
+        //now add the list to the xml file
+        classesForDayListView = (ListView) findViewById(R.id.classesForDayView);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, classPerDayList);
+        classesForDayListView.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/purdueplanner/purdueplanner/ScheduleActivity.java
+++ app/src/main/java/com/purdueplanner/purdueplanner/ScheduleActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.format.DateUtils;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +24,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 
 /**
@@ -56,12 +58,46 @@ public class ScheduleActivity extends FragmentActivity {
                 view.findViewById(R.id.calendarView);
                 SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy"); //makes the date format for the date - month, day, and year
                 Calendar c = Calendar.getInstance(); //makes an instance of a calendar object
-                c.set(year,month,dayOfMonth); //sets the year, month and date based on what the user selected
+                c.set(year, month, dayOfMonth); //sets the year, month and date based on what the user selected
                 String currdate = dateFormat.format(c.getTime()); //formats the date based on our date format
-                TextView dateText = (TextView)findViewById(R.id.selected_date); //the
+                TextView dateText = (TextView)findViewById(R.id.selected_date); //sets the date text to the one on our xml file
                 dateText.setText(currdate);
-                startActivity(new Intent(ScheduleActivity.this, DayScheduleActivity.class)); //goes to new activity once the button is pressed
+
+                //Gets the day from the date
+                TextView dayText = (TextView)findViewById(R.id.selected_day); //sets the day to the first letter abbreviation
+                SimpleDateFormat newDateFormat = new SimpleDateFormat("EEEEE", Locale.getDefault()); //Formats the day to display the full word.. i.e. "Monday"
+                String actualDay = newDateFormat.format(c.getTime()); //gets the time to adhere to the format
+
+                //Since we got the day, let's make it match the databases one letter formatting
+                String letter = "";
+                switch(actualDay){
+                    case "Monday": letter = "M";
+                        break;
+                    case "Tuesday": letter = "T";
+                        break;
+                    case "Wednesday": letter = "W";
+                        break;
+                    case "Thursday": letter = "R";
+                        break;
+                    case "Friday": letter = "F";
+                        break;
+                    case "Saturday": letter = "S";
+                        break;
+                    case "Sunday": letter = "U";
+                        break;
+                    //Supposedly there is no classes offered on Sunday's at Purdue, so I won't account for it
+                }
+
+                dayText.setText(letter);
+
+                //in order to pass the 'currdate' (and the day letter) string in using a new Activity, we have to do this
+                Intent myIntent = new Intent(ScheduleActivity.this, DayScheduleActivity.class);
+//                myIntent.putExtra("currdate_key", currdate); //adds the string to a HashMap like object
+                myIntent.putExtra("dayletter_key", letter);
+                startActivity(myIntent); //goes to new activity once the button is pressed
             }
+
+
 
         });
 
